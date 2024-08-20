5c031df5349ec341a86c3d2a80812e600fc85cc1
LishOleG <lisholeg16@gmail.com>
2024-08-20 22:20:34 +0500

    successful completion app.properties

---
+++ src/main/java/Spring_MVC_Hibernate_in_SpringBoot/CRUD_SpringBoot/CrudSpringBootApplication.java
@@ -0,0 +1,13 @@
+package Spring_MVC_Hibernate_in_SpringBoot.CRUD_SpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Spring_MVC_Hibernate_in_SpringBoot/CRUD_SpringBoot/CrudSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package Spring_MVC_Hibernate_in_SpringBoot.CRUD_SpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
